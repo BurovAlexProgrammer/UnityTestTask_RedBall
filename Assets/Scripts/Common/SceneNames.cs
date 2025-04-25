e99fce141cec0298f6b49c4e83cb3b1aabd91bf3
Aleksey Burov <burovalexprogrammer@gmail.com>
2025-04-25 08:28:08 +0200

    Boot scene

---
+++ Assets/Scripts/Common/SceneNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Common
+{
+    public static class SceneNames
+    {
+        public const string Boot = "Boot";
+        public const string MainMenu = "MainMenu";
+        public const string GameScene = "GameScene";
+    }
+}
